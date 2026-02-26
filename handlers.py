f340d65e387191da813494c7d71d413b21016bac
Алексей <aleks120825@gmail.com>
2026-02-26 20:40:56 +0300

    Исправление багов, оптимизация с помощью FSMContext

--- handlers.py
+++ handlers.py
@@ -1,14 +1,17 @@
 from aiogram.types import CallbackQuery, FSInputFile, Message
 from aiogram.filters import Command
 from aiogram import F
+from aiogram.fsm.context import FSMContext
 from database import add_favorite, get_favorites, remove_favorite
-from keyboards import start_keyboard, genre_keyboard, length_keyboard, type_keyboard, favorite_keyboard, remove_keyboard
+from keyboards import (
+    start_keyboard, genre_keyboard, 
+    length_keyboard, type_keyboard, 
+    favorite_keyboard, remove_keyboard)
 import json, random
 
-
-user_data = {}
-
 def register_handlers(dp):
+    with open("games.json", "r", encoding="utf-8") as f:
+        games = json.load(f)
     @dp.message(Command("start"))
     async def cmd_start(message: Message):
         await message.answer("Хочешь подбирать игру?", reply_markup=start_keyboard)
@@ -33,7 +36,7 @@ def register_handlers(dp):
     async def show_favorites(message: Message):
         favorites = get_favorites(message.from_user.id)
         if not favorites:
-            await message.answer("Избранное пуста, добавляй игры 🎮")
+            await message.answer("Избранное пусто, добавляй игры 🎮")
         else:
             for title in favorites:
                 await message.answer(title, reply_markup=remove_keyboard(title))
@@ -42,17 +45,16 @@ def register_handlers(dp):
     async def remove_from_favorite(callback: CallbackQuery):
         title = callback.data.replace("remove_", "")
         remove_favorite(callback.from_user.id, title)
-        await callback.answer(f"'{title}' Удалено ❌")       
+        await callback.answer(f"'{title}' Удалено ❌")     
+        await callback.message.delete()  
  
     @dp.callback_query(F.data.startswith("genre_"))
-    async def genre_selected(callback: CallbackQuery):
+    async def genre_selected(callback: CallbackQuery, state: FSMContext):
         await callback.answer()
         genre = callback.data.replace("genre_", "")
-        user_data[callback.from_user.id] = {"genre": genre} 
+        await state.update_data(genre=genre)
         
         if genre in ["fighting", "racing"]:
-            with open("games.json", "r", encoding="utf-8") as f:
-                games = json.load(f)
             matches = [g for g in games if g["genre"] == genre]
             if matches:
                 game = random.choice(matches)
@@ -61,28 +63,31 @@ def register_handlers(dp):
                     caption=f"название: {game['title']}\n\n{game['description']}",
                     reply_markup=favorite_keyboard(game["title"])
                 )
+            await state.clear()
             return 
                 
         await callback.message.answer("Выбери длительность прохождения игры", reply_markup=length_keyboard)
         
     @dp.callback_query(F.data.startswith("length_"))
-    async def length_selected(callback: CallbackQuery):
+    async def length_selected(callback: CallbackQuery, state: FSMContext):
         await callback.answer()
         length= callback.data.replace("length_", "")
-        user_data[callback.from_user.id]["length"] = length
+        await state.update_data(length=length)
         await callback.message.answer("Выбери тип игры", reply_markup=type_keyboard)
         
     @dp.callback_query(F.data.startswith("type_"))
-    async def type_selected(callback: CallbackQuery):
+    async def type_selected(callback: CallbackQuery, state: FSMContext):
         await callback.answer()
         game_type = callback.data.replace("type_", "")
-        user_data[callback.from_user.id]["type"] = game_type
-        data = user_data.get(callback.from_user.id, {})
-        with open("games.json", "r", encoding="utf-8") as f:
-            games = json.load(f)
-        matches = [g for g in games if g["genre"] == data["genre"] and g["length"] == data["length"] and g["type"].lower() == data["type"]]
-        game = random.choice(matches)
+        await state.update_data(game_type=game_type)
+        data = await state.get_data()
+        await state.clear()
+        if "genre" not in data or "length" not in data:
+            await callback.message.answer("Что-то пошло не так 🤔\n\nНачни заново\n\n/start")
+            return
+        matches = [g for g in games if g["genre"] == data["genre"] and g["length"] == data["length"] and g["type"].lower() == game_type]
         if matches:
+            game = random.choice(matches)
             await callback.message.answer_photo(
                 photo=FSInputFile(game["image"]),
                 caption=f"Название: {game['title']}\n\n{game['description']}",
